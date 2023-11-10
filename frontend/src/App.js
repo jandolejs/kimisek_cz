c552986261ff6c6135a4273d1ace6c7ffc9bb7c0
Jan Dolejs <jandolejs1999@gmail.com>
2023-11-10 12:40:37 +0100

    change class to classname

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-      <div class="row">
-          <div class="column">
+      <div className="row">
+          <div className="column">
               <img src="https://cdn.jandolejs.cz/b23f81db787ad30fadeb5df94d89d0be " className="img" alt="kimi_here"></img>
           </div>
-          <div class="column">
+          <div className="column">
               <a href="https://www.instagram.com/life_ruined_by"><img src="https://cdn.jandolejs.cz/d0a870829fa7b8d8da9ec723190407bf" className="img" alt="kimi_famous"></img></a>
           </div>
-          <div class="column">
+          <div className="column">
               <img src="https://cdn.jandolejs.cz/68e850da2f4487f4bb352aae7785ebf2" className="img" alt="kimi_there"></img>
           </div>
       </div>
