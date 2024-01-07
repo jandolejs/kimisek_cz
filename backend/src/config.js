3b43c127a7a67e858fd1680751def38d0da5f0b8
Jan Dolejs <jandolejs1999@gmail.com>
2024-01-07 18:25:43 +0100

    remove backend

--- backend/src/config.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-const readFileSync = filename => fs.readFileSync(filename).toString("utf8");
-
-module.exports = {
-  port: process.env.PORT || 8080
-};
--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", function(req, res, next) {
-  res.send("Hi\n");
-});
-
-module.exports = app;
